b7bc2ed4cfbd535be4ac59828dec5ccc7bd4aa12
Clifford Bamert <cliff.bam92@gmail.com>
2024-02-13 07:00:55 -0600

    first commit

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:34ca0a3e5cf866ef940b78f37678952abaee14c9ec813fd7f68db4d98f9d15f6
+size 442
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c313ed9c1656cf22eb8a4f75fc24842217a4da0985cd46619d7d05806b833c8
+size 3196
